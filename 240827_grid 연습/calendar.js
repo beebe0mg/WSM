8abc58cded581ef9402bbeac7cebcaa225dc5b58
정상미 <w2321@e-mirim.hs.kr>
2024-09-10 15:57:12 +0900

    css grid 연습 .이전/다음 달 버튼 클릭 처리

--- 240827_grid 연습/calendar.js
+++ 240827_grid 연습/calendar.js
@@ -2,6 +2,7 @@
 
 // 현재 날짜 구하자
 let currentDate = new Date();      // 현재 날짜 구하기
+
 const setCalendarHeader = (date) => {   // 날짜를 구하는 함수에게로 던짐
     // 연도 구하자
     const year = date.getFullYear();
@@ -15,8 +16,31 @@ const setCalendarHeader = (date) => {   // 날짜를 구하는 함수에게로 �
     // innerHTML 을 해서 titleString 을 넣어준다.
 }
 
-setCalendarHeader(currentDate);
+const changeMonth = (delta) => {
+    currentDate.setMonth(currentDate.getMonth() + delta);
+    setCalendarHeader(currentDate);
+}
+
+// 이전 달 버튼 이벤트 처리하자
+const prevMonthButton = document.getElementById("prev-month");
+// 지금 html 의 id prev-month 를 js 로 가져옴
+prevMonthButton.addEventListener("click", () => changeMonth(-1));
+// 버튼을 눌렀을 때의 이벤트 처리
+// prevMonthButton.addEventListener("click", console.log('이번 달')); --> 하면 안 됨.
+// console.log() 함수 실행한 결과를 클릭했을 때 실행하는 것임 즉, 아무 일도 안 함
+// 상미야 라고 부르면 대답해야 하는데 () => 이걸 작성 안 해주면 상미야 하고 그냥 끝남..
+// 그래서 함수의 꼴로 작성해 줘야 함 : 여기서는 익명 함수
+
+// 다음 달 버튼 이벤트 처리하자
+const nextMonthButton = document.querySelector("#next-month");
+// querySelector 로 가져옴
+nextMonthButton.onclick = () => changeMonth(1);
+// onclick = addEventListener
+
 // 일 구하자
+const setCalendar = (date) => {
+
+}
 
 // 첫 날의 요일 구하자
 // 마지막 날짜 구하자
